26e0f0c62c5b06d03126313a111d72ac26efa946
Backflipper666 <elderly619@gmail.com>
2022-06-12 00:27:21 +0600

    make button responsive in desktop screen
    button only hides 3 images when it comes to tablet screens, as opposed to 5

--- 1.5/script.js
+++ 1.5/script.js
@@ -20,3 +20,32 @@ const swiper = new Swiper('.swiper', {
 
   
 });
+
+let button = document.querySelector("button.btn");
+//.labels__lenovo2, .labels__samsung2, .labels__apple2
+let lenovo2 = document.querySelector("img.labels__lenovo2");
+let apple2 = document.querySelector("img.labels__apple2");
+let samsung2 = document.querySelector("img.labels__samsung2");
+let expand = document.querySelector("img.btn__expand");
+let text = document.querySelector("span.btn__text");
+
+
+
+button.addEventListener("click", ()=>{
+  if (text.textContent == "Скрыть"){
+    lenovo2.setAttribute("style", "visibility:hidden; display:none;");
+    apple2.setAttribute("style", "visibility:hidden; display:none;");
+    samsung2.setAttribute("style", "visibility:hidden; display:none;");
+    expand.setAttribute("style", "transform:rotate(180deg);")
+    text.textContent = "Показать все";
+  }
+  else{
+    lenovo2.setAttribute("style", "visibility:visible; display:inline-block;");
+    apple2.setAttribute("style", "visibility:visible; display:inline-block;");
+    samsung2.setAttribute("style", "visibility:visible; display:inline-block;");
+    expand.setAttribute("style", "transform:rotate(360deg);")
+    text.textContent = "Скрыть";
+  }
+})
+
+
